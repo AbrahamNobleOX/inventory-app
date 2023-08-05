6a2352076dcbaed50bc115dbbffdf07967f816b1
Akintola Abraham <abramseyakid@gmail.com>
2023-08-05 22:53:19 +0100

    Server configuration

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json("Home Page");
+});
+
+// Connect to DB and start server
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
