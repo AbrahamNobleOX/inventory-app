69821a8f9e880267573e285bb94d01a8d6b1181f
Akintola Abraham <abramseyakid@gmail.com>
2023-08-14 01:11:56 +0100

    Added reset page and layout

--- frontend/src/pages/auth/Reset.js
+++ frontend/src/pages/auth/Reset.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./auth.module.scss";
+import { MdPassword } from "react-icons/md";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  password: "",
+  password2: "",
+};
 
 const Reset = () => {
-  return <div>Reset</div>;
+  const [formData, setformData] = useState(initialState);
+  const { password, password2 } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setformData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <MdPassword size={35} color="#999" />
+          </div>
+          <h2>Reset Password</h2>
+
+          <form>
+            <input
+              type="password"
+              placeholder="New Password"
+              required
+              name="password"
+              value={password}
+              onChange={handleInputChange}
+            />
+            <input
+              type="password"
+              placeholder="Confirm New Password"
+              required
+              name="password2"
+              value={password2}
+              onChange={handleInputChange}
+            />
+
+            <button type="submit" className="--btn --btn-primary --btn-block">
+              Reset Password
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to="/">- Home</Link>
+              </p>
+              <p>
+                <Link to="/login">- Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Reset;
