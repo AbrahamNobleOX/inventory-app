88fe7ac6c062ad91f7b2eda8278451d9631001d7
Akintola Abraham <abramseyakid@gmail.com>
2023-08-17 00:37:57 +0100

    Added loader component

---
+++ frontend/src/components/loader/Loader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import loaderImg from "../../assets/loader.gif";
+import ReactDOM from "react-dom";
+import "./Loader.scss";
+
+// Full screen loader
+const Loader = () => {
+  return ReactDOM.createPortal(
+    <div className="wrapper">
+      <div className="loader">
+        <img src={loaderImg} alt="Loading..." />
+      </div>
+    </div>,
+    document.getElementById("loader")
+  );
+};
+
+// Element Loader image
+export const SpinnerImg = () => {
+  return (
+    <div className="--center-all">
+      <img src={loaderImg} alt="Loading..." />
+    </div>
+  );
+};
+
+export default Loader;
