3a9fcfa867a0d5ecbe0557f0992b544b4341abe9
Akintola Abraham <abramseyakid@gmail.com>
2023-08-18 02:58:30 +0100

    Connected login status and added session check

--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React from "react";
+import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 
 const Dashboard = () => {
+  useRedirectLoggedOutUser("/login");
   return <h2>Dashboard</h2>;
 };
 
