8d75217da3b9c25c652e7075cc18789bc69c9ae7
Akintola Abraham <abramseyakid@gmail.com>
2023-08-23 12:39:54 +0100

    Added dashboard products list

--- frontend/src/components/product/productList/ProductList.js
+++ frontend/src/components/product/productList/ProductList.js
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { SpinnerImg } from "../../loader/Loader";
+import "./productList.scss";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { AiOutlineEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-const ProductList = () => {
-  return <div>ProductList</div>;
+const ProductList = ({ products, isLoading }) => {
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      const shortenedText = text.substring(0, n).concat("...");
+      return shortenedText;
+    }
+    return text;
+  };
+
+  return (
+    <div className="product-list">
+      <hr />
+      <div className="table">
+        <div className="--flex-between --flex-dir-column">
+          <span>
+            <h3>Inventory Items</h3>
+          </span>
+          <span>
+            <h3>Search Products</h3>
+          </span>
+        </div>
+
+        {isLoading && <SpinnerImg />}
+
+        <div className="table">
+          {!isLoading && products.length === 0 ? (
+            <p>-- No product found, please add a product...</p>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>s/n</th>
+                  <th>Name</th>
+                  <th>Category</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Value</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {products.map((product, index) => {
+                  const { _id, name, category, price, quantity } = product;
+                  return (
+                    <tr key={_id}>
+                      <td>{index + 1}</td>
+                      <td>{shortenText(name, 16)}</td>
+                      <td>{category}</td>
+                      <td>
+                        {"$"}
+                        {price}
+                      </td>
+                      <td>{quantity}</td>
+                      <td>
+                        {"$"}
+                        {price * quantity}
+                      </td>
+                      <td className="icons">
+                        <span>
+                          <Link to={`/product-detail/${_id}`}>
+                            <AiOutlineEye size={25} color={"purple"} />
+                          </Link>
+                        </span>
+                        <span>
+                          <Link to={`/edit-product/${_id}`}>
+                            <FaEdit size={20} color={"green"} />
+                          </Link>
+                        </span>
+                        <span>
+                          <FaTrashAlt
+                            size={20}
+                            color={"red"}
+                            onClick={() => confirmDelete(_id)}
+                          />
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
