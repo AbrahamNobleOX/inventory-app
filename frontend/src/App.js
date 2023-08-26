72f8b191840c9aaf37423bea9c8fbdca4755c15b
Akintola Abraham <abramseyakid@gmail.com>
2023-08-26 12:08:35 +0100

    Added contactus page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import EditProduct from "./pages/editProduct/EditProduct";
 import ProductDetail from "./components/product/productDetail/ProductDetail";
 import Profile from "./pages/profile/Profile";
 import EditProfile from "./pages/profile/EditProfile";
+import Contact from "./pages/contact/Contact";
 
 axios.defaults.withCredentials = true;
 
@@ -104,6 +105,17 @@ function App() {
             <Sidebar>
               <Layout>
                 <EditProfile />
+              </Layout>
+            </Sidebar>
+          }
+        />
+
+        <Route
+          path="/contact-us"
+          element={
+            <Sidebar>
+              <Layout>
+                <Contact />
               </Layout>
             </Sidebar>
           }
